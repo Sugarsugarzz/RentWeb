b73af9ca7ca272e073cc0ba66a2dcab095c086d2
Sugarbabyzz <406857586@qq.com>
2019-05-10 17:50:39 +0800

    Update HouseController.java

--- src/main/java/com/sugar/rent/controller/HouseController.java
+++ src/main/java/com/sugar/rent/controller/HouseController.java
@@ -6,9 +6,10 @@ import com.sugar.rent.service.HouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
+
 
 @Controller
 public class HouseController {
@@ -37,14 +38,41 @@ public class HouseController {
      * @return
      */
     @RequestMapping("/main")
-    public String main(Model model) {
+    public String main(ModelMap model) {
 
-        List<House> list = houseService.getHouseListAtBj();
+        List<House> listBj = houseService.getHouseListAtBj();
+        List<House> listCd = houseService.getHouseListAtCd();
+        List<House> listCq = houseService.getHouseListAtCq();
+        List<House> listCs = houseService.getHouseListAtCs();
+        List<House> listGz = houseService.getHouseListAtGz();
+        List<House> listHf = houseService.getHouseListAtHf();
+        List<House> listHz = houseService.getHouseListAtHz();
+        List<House> listNj = houseService.getHouseListAtNj();
+        List<House> listQd = houseService.getHouseListAtQd();
+        List<House> listSh = houseService.getHouseListAtSh();
+        List<House> listSz = houseService.getHouseListAtSz();
+        List<House> listTj = houseService.getHouseListAtTj();
+        List<House> listWh = houseService.getHouseListAtWh();
+        List<House> listXa = houseService.getHouseListAtXa();
+        List<House> listXm = houseService.getHouseListAtXm();
 
-        model.addAttribute("houses", list);
+
+        model.addAttribute("housesBj", listBj);
+        model.addAttribute("housesCd", listCd);
+        model.addAttribute("housesCq", listCq);
+        model.addAttribute("housesCs", listCs);
+        model.addAttribute("housesGz", listGz);
+        model.addAttribute("housesHf", listHf);
+        model.addAttribute("housesHz", listHz);
+        model.addAttribute("housesNj", listNj);
+        model.addAttribute("housesQd", listQd);
+        model.addAttribute("housesSh", listSh);
+        model.addAttribute("housesSz", listSz);
+        model.addAttribute("housesTj", listTj);
+        model.addAttribute("housesWh", listWh);
+        model.addAttribute("housesXa", listXa);
+        model.addAttribute("housesXm", listXm);
 
         return "main.jsp";
     }
-
-
 }
